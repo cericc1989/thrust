42fa8979b3d0e541488ccbfb5803cb5ff1d0d015
chriscohran <chriscohran@me.com>
2016-08-02 21:09:12 -0400

    First commit

---
+++ build/Thrust_.build/Release-iphoneos/Thrust_.build/Objects-normal/armv7/arch.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/chriscohran/GameMaker-Studio/Thrust!/Thrust_/Thrust_/Game/arch.mm \
+  /Users/chriscohran/GameMaker-Studio/Thrust!/Thrust_/Thrust_/Supporting\ Files/Thrust_-Prefix.pch
